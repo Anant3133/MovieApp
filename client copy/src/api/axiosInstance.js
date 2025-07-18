62154571223e71456975c8e98d2482bcd91918e6
Anant Mathur <anantmg10@gmail.com>
2025-07-19 00:45:28 +0530

    Render deployment

--- client copy/src/api/axiosInstance.js
+++ client copy/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '../utils/token';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://https://movieapp-u9ts.onrender.com/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
